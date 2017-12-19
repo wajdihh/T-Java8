7129f27a380ab8e4330748b5d60c3a78e806b378
wajdihh <wajdihh@gmail.com>
2017-12-19 01:50:42 +0100

    2 commit

--- src/com/hh/training/Main.java
+++ src/com/hh/training/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("hello wajdi Hh");
+        System.out.println("hello wajdi Hh 2");
     }
 }
